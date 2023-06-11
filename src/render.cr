32684c298576e47a5580de4306141949a9774ce2
Roberto Alsina <roberto.alsina@gmail.com>
2023-06-11 08:35:31 -0300

    Use current croupier API

--- src/render.cr
+++ src/render.cr
@@ -15,17 +15,15 @@ module Render
       end
 
       output = "output/#{post.@link}"
-      render = ->{
-        Util.log("    #{output}")
-        Render.write(post.rendered, template, output)
-      }
-
       name = "render #{output}"
       Croupier::Task.new(
         name: name,
         output: output,
         inputs: ["conf", post.@source, post.template],
-        block: render
+        proc: ->{
+          Util.log("    #{output}")
+          apply_template(post.rendered, template)
+        }
       )
     end
   end
@@ -34,40 +32,31 @@ module Render
     inputs = ["conf"] + posts.map { |post| post.@source }
     name = "render #{output}"
 
-    render = ->{
-      Util.log("    #{output}")
-      feed = RSS.new title: title
-      posts.each do |post|
-        feed.item(
-          title: post.@title,
-          link: post.@link,
-          pubDate: post.date.to_s
-        )
-      end
-      Dir.mkdir_p(File.dirname output)
-      File.open(output, "w") do |io|
-        io.puts feed.xml indent: true
-      end
-    }
-
     Croupier::Task.new(
       name: name,
       output: output,
       inputs: inputs,
-      block: render
+      proc: ->{
+        Util.log("    #{output}")
+        feed = RSS.new title: title
+        posts.each do |post|
+          feed.item(
+            title: post.@title,
+            link: post.@link,
+            pubDate: post.date.to_s
+          )
+        end
+        feed.xml indent: true
+      }
     )
   end
 
   # Writes html, templated properly with config, into path
-  def self.write(html, template, path)
+  def self.apply_template(html, template)
     # TODO: use a copy of config
-    rendered_page = Crustache.render(template,
+    Crustache.render(template,
       Config.config.merge({
         "content" => html,
       }))
-    Dir.mkdir_p(File.dirname path)
-    File.open(path, "w") do |io|
-      io.puts rendered_page
-    end
   end
 end
