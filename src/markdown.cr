92e268f6770d757931b462e5a46f117a30577653
Roberto Alsina <roberto.alsina@gmail.com>
2023-07-07 12:56:49 -0300

    A bit less noisy

--- src/markdown.cr
+++ src/markdown.cr
@@ -58,7 +58,7 @@ module Markdown
       Log.info { "Reading Markdown from #{path}" }
       posts = [] of File
       Dir.glob("#{path}/**/*.md").each do |p|
-        Log.info { "<< #{p}" }
+        Log.debug { "<< #{p}" }
         posts << File.new(p)
       end
       posts
