006f198e354ad77ae07104d8e0f25f0ddc2e39e4
Roberto Alsina <roberto.alsina@gmail.com>
2023-07-07 12:55:57 -0300

    Fix folders in assets

--- src/assets.cr
+++ src/assets.cr
@@ -2,6 +2,7 @@ module Assets
   # Copy assets from assets/ to output/
   def self.render
     Dir.glob("assets/**/*").each do |src|
+      next if File.directory?(src)
       dest = Path.new(["output"] + Path[src].parts[1..])
       Croupier::Task.new(
         output: dest.to_s,
