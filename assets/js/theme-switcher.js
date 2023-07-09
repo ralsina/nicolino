b5c68e494ede9518952cd5327e367392aa68b8cd
Roberto Alsina <roberto.alsina@gmail.com>
2023-07-09 14:02:27 -0300

    Default to dark theme

--- assets/js/theme-switcher.js
+++ assets/js/theme-switcher.js
@@ -1,12 +1,11 @@
-let isLight = true
+let isLight = false
 const html = document.documentElement
 const switchTheme = document.getElementById('switch_theme')
 
 document.addEventListener('DOMContentLoaded', () => {
-  html.setAttribute('data-theme', 'light')
+  html.setAttribute('data-theme', 'dark')
 })
 switchTheme.addEventListener('click', (e)=> {
   isLight = !isLight
   html.setAttribute('data-theme', isLight? 'light':'dark')
-  removeTooltip()
 })
