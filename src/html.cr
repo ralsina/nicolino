9c4ab3271145b332295469e3a4144d7a45da20c4
Roberto Alsina <roberto.alsina@gmail.com>
2023-08-05 23:06:11 -0300

    HTML compiler probably ok

--- src/html.cr
+++ src/html.cr
@@ -3,12 +3,12 @@ require "./markdown"
 module HTML
   # Posts written directly in HTML
   class File < Markdown::File
-    def html
-      lang = Locale.language
+    def html(lang = nil)
+      lang ||= Locale.language
       # FIXME: Implement TOC using lexbor
-      @html[lang] = replace_shortcodes
-      @html[lang] = HtmlFilters.downgrade_headers(html)
-      @html[lang] = HtmlFilters.make_links_absolute(html, link)
+      @html[lang] = replace_shortcodes(lang)
+      @html[lang] = HtmlFilters.downgrade_headers(html(lang))
+      @html[lang] = HtmlFilters.make_links_absolute(html(lang), link)
     end
   end
 
@@ -17,12 +17,12 @@ module HTML
   def self.read_all(path)
     Log.info { "Reading HTML files from #{path}" }
     posts = [] of File
-    Dir.glob("#{path}/**/*.html").each do |p|
+    all_sources = Utils.find_all(path, "html")
+    all_sources.map do |base, sources|
       begin
-        # FIXME: do properly
-        posts << File.new({"en" => p}, Path[p])
+        posts << File.new(sources, base)
       rescue ex
-        Log.error { "Error parsing #{p}: #{ex.message}" }
+        Log.error { "Error parsing #{base}: #{ex.message}" }
         Log.debug { ex }
       end
     end
