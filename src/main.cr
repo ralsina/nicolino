97cb180d680f395eea202cec876af9d447037d1d
Roberto Alsina <roberto.alsina@gmail.com>
2024-10-07 12:34:18 -0300

    fix: better version/help in CLI

--- src/main.cr
+++ src/main.cr
@@ -1,4 +1,12 @@
 require "./commands/*"
 require "./*"
 
+exit Polydocopt.main("faaso", ["--help"]) if ARGV.empty?
+cmdname = ARGV[0]
+
+if cmdname == "version"
+  puts "nicolino #{VERSION}"
+  exit 0
+end
+
 exit(Polydocopt.main("nicolino", ARGV))
