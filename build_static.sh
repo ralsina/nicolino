bf2b87d2c406da307f2986a218b03e88214d0065
Roberto Alsina <roberto.alsina@gmail.com>
2026-01-15 06:10:55 -0300

    build: disable ARM

--- build_static.sh
+++ build_static.sh
@@ -11,6 +11,6 @@ docker run -ti --rm -v "$PWD":/app --user="$UID" nicolino-builder /bin/sh -c "cd
 mv bin/nicolino bin/nicolino-static-linux-amd64
 
 # Build for ARM64
-docker build . -f Dockerfile.static --platform linux/arm64 -t nicolino-builder
-docker run -ti --rm -v "$PWD":/app --platform linux/arm64 --user="$UID" nicolino-builder /bin/sh -c "cd /app && rm -rf lib && shards build -Dnovips --static --release --without-development && strip bin/nicolino"
-mv bin/nicolino bin/nicolino-static-linux-arm64
+#docker build . -f Dockerfile.static --platform linux/arm64 -t nicolino-builder
+#docker run -ti --rm -v "$PWD":/app --platform linux/arm64 --user="$UID" nicolino-builder /bin/sh -c "cd /app && rm -rf lib && shards build -Dnovips --static --release --without-development && strip bin/nicolino"
+#mv bin/nicolino bin/nicolino-static-linux-arm64
